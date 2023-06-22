0f664e65bad11e3d73c096c419fa1e72b516011c
PacoSalazar <pa_ko_7@hotmail.com>
2023-06-22 12:57:12 -0600

    Initial commit

---
+++ src/main/java/com/psalazar/awscp/AwscpApplication.java
@@ -0,0 +1,13 @@
+package com.psalazar.awscp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwscpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwscpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psalazar/awscp/AwscpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psalazar.awscp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwscpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
